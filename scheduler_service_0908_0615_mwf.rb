a599a31e89e7a1e95fcf886675db1cdfce22c25e
Ro948YAl <brandy.merger_0t@icloud.com>
2025-09-08 07:15:23 +0900

    Update RUBY 定时任务调度器

---
+++ scheduler_service_0908_0615_mwf.rb
@@ -0,0 +1,54 @@
+# 代码生成时间: 2025-09-08 06:15:22
+# 定时任务调度器
+#
+# 该服务使用RUBY和CUBA框架实现定时任务调度功能
+#
+# @author: 你的名字
+# @date: 2023-04-01
+
+require 'cuba'
+require 'rufus-scheduler'
+
+# 导入CUBA框架和Rufus-scheduler库
+class SchedulerService < Cuba
+  # 设置调度器
+  scheduler = Rufus::Scheduler.start_new
+  
+  # 定义定时任务调度器
+  on get do
+    on "/schedule" do
+      # 获取任务ID和执行时间
+      task_id = params['task_id']
+      time = params['time']
+      
+      begin
+        # 检查任务ID和时间参数是否有效
+        unless task_id && time
+          throw 'Invalid parameters'
+        end
+        
+        # 将时间参数转换为时间对象
+        schedule_time = Time.parse(time)
+        
+        # 在调度器中添加定时任务
+        scheduler.in '1s' do # 等待1秒后执行任务，模拟任务调度过程
+          puts "Executing task #{task_id} at #{schedule_time}"
+        end
+        
+        # 返回成功消息
+        res.write "Task #{task_id} scheduled at #{schedule_time}"
+      rescue => e
+        # 错误处理
+        res.write "Error: #{e.message}"
+      end
+    end
+  end
+
+  # 定义根路径，返回欢迎信息
+  on root do
+    res.write "Welcome to the Scheduler Service"
+  end
+end
+
+# 启动CUBA服务器
+run SchedulerService, Cuba::Logger::DEBUG
